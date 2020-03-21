6b2fed3927b1d11777f04b1c33fb902622c1780d
Shrey <shrey@shrey-HP-ProBook-440-G1>
2020-03-21 10:39:49 +0530

    initial commit

---
+++ comingsoon_06/form1.php
@@ -0,0 +1,46 @@
+<?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "shreysharma9628@gmail.com";
+$mail->Password   = "yoyohoney@12345";
+$mail->IsHTML(true);
+$mail->AddAddress("shreysharma96280@gmail.com", "recipient-name"); //Jisse bhejni h
+$mail->SetFrom("shreysharma9628@gmail.com", "Shrey Sharma");
+//$mail->AddReplyTo("reply-to-email@domain", "reply-to-name");
+$mail->AddCC("shreysharma9628@gmail.com", "Auto Generated mail");
+$mail->Subject = "Thank you for contacting US";
+$name = $_POST['name']; // required
+$email_from = $_POST['email']; // required
+$number= $_POST['number']; // required
+
+ function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+
+    $email_message .= "<b>Name:</b> ".clean_string($name)."<br>";
+    $email_message .= "<b>Number:</b> ".clean_string($number)."<br>";
+    $email_message .= "<b>Email:</b> ".clean_string($email_from)."<br>";
+
+//$content = "<b>Name</b>"+$name;
+$mail->MsgHTML($email_message); 
+if(!$mail->Send()) {
+
+ echo "<script>alert('Something went wrong !!');document.location='http://localhost/comingsoon_06/'</script>";
+
+  var_dump($mail);
+} else {
+ echo "<script>alert('We got you response...Thank you !!');document.location='http://localhost/comingsoon_06/'</script>";
+}
+?>
